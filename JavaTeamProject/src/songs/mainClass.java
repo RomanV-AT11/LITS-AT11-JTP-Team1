85641f04edd2f44c126fc71431221a72263d1dac
Nataliyas <7nataly7@ukr.net>
2015-09-12 18:40:10 +0300

    Parse fixing 1.0

--- JavaTeamProject/src/songs/mainClass.java
+++ JavaTeamProject/src/songs/mainClass.java
@@ -16,10 +16,35 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
+
+
+import java.io.FileReader;
+import java.util.Iterator;
+ 
+
 public class mainClass {
 
 		public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
-//
+		
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+	        Collection<SongsNS> songs = null;
+	        try {        	
+	            songs = mapper.readValue(new File("intputfile.json"), 
+	            		mapper.getTypeFactory().constructCollectionType(List.class, SongsNS.class));
+	        } catch (IOException e) {
+	            System.out.println("Cannot load input JSON file");
+	            //e.printStackTrace();
+	            return;
+	        }
+	        //Collections.sort(songs);		//Sorting
+	        for (SongsNS song : songs) {
+	            System.out.println(song);
+	        }
+
+	        //
 //			File jsonFile = new File("inputfile.json");
 //		
 //			SongsNS Songs = null; 
@@ -29,38 +54,6 @@ public class mainClass {
 //
 //		    Songs = mapper.readValue(jsonFile, SongsNS.class);
 //		    System.out.println(Songs.getName());
-
-			ObjectMapper mapper = new ObjectMapper();
-
-	        Collection<SongsNS> songs = null;
-	        try {
-	        	
-	            songs = mapper.readValue(new File("intputfile.json"), mapper.getTypeFactory().constructCollectionType(List.class, SongsNS.class));
-	        } catch (IOException e) {
-	            System.out.println("Cannot load data");
-	            //e.printStackTrace();
-	            return;
-	        }
-	        //Collections.sort(songs);
-	        for (SongsNS song : songs) {
-	            System.out.println(song);
-	        }
-
-//			JSONObject obj;
-//			try {
-//				obj = new JSONObject("{interests : [{interestKey:Dogs}, {interestKey:Cats}]}");
-//				List<String> list = new ArrayList<String>(); 
-//				JSONArray array = obj.getJSONArray("Song"); 
-//
-//				for(int i = 0 ; i < array.length() ; i++){ 
-//					list.add(array.getJSONObject(i).getString("interestKey")); 
-//					System.out.println(list.get(i));
-//				} 
-//			} catch (JSONException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			} 
-		  
 			
 	}
 
